3f4003849ef81e8c226555ce965159f4dea8291e
Ben Parisot <me@benparisot.com>
2019-05-05 19:32:32 -0700

    finish hello world python app

---
+++ program.py
@@ -0,0 +1,8 @@
+print('----------------')
+print('   Oh hello!')
+print('----------------')
+print()
+
+user_name = input('What is your name? ')
+greeting = 'Nice to mee you ' + user_name
+print(greeting)
